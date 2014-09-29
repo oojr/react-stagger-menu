c1ad000da87c81be42e7dd16bb7331243c9444d9
Olajide Ogundipe Jr <o.ogundipejr@gmail.com>
2014-09-29 17:40:38 -0400

    gh-pages

--- __app/index.js
+++
@@ -1,5 +0,0 @@
-/** @jsx React.DOM */
-var StaggerMenu = require('./components/StaggerMenu');
-var react = require('react');
-var el = document.getElementById('stagger-menu');
-react.renderComponent(<StaggerMenu />, el);
